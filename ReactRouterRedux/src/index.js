4f74e56ff1aa75bac1608ca88c3dfdd550fdd1f5
titanXL <emildavidkov88@gmail.com>
2016-09-06 20:54:30 +0300

    ReactRoute

---
+++ ReactRouterRedux/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes'
+import reducers from './reducers';
+import promice from 'redux-promise'
+const createStoreWithMiddleware = applyMiddleware(
+  promice
+)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>
+  , document.querySelector('.container'));
